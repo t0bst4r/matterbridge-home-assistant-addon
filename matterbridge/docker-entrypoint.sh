a911e22ab754303bc7195835fd1a38314ed1ddaa
Gene Hand <gene.hand@gmail.com>
2024-06-02 15:31:39 -0700

    use the websocket proxy & auth

--- matterbridge/docker-entrypoint.sh
+++ matterbridge/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv bashio
 
-HOME_ASSISTANT_URL=$(bashio::config 'home_assistant_url')
-HOME_ASSISTANT_ACCESS_TOKEN=$(bashio::config 'home_assistant_access_token')
+HOME_ASSISTANT_URL=http://supervisor/core
+HOME_ASSISTANT_ACCESS_TOKEN=$SUPERVISOR_TOKEN
 HOME_ASSISTANT_CLIENT_CONFIG=$(bashio::config 'home_assistant_client_config')
 
 export HOME_ASSISTANT_URL
