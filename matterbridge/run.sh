977c7bc7c453bb4694c7b849981eeb3cf3544bd7
Gene Hand <genehand@users.noreply.github.com>
2024-06-05 13:44:06 -0700

    feat: proper addon configuration using homeassistant_api and config volume (#2)
    * rework to use the HA base images
    
    * use the websocket proxy & auth
    
    * persistent storage
    
    * restore the image config
    
    * change back from my fork
    
    * disable the release on merge for now
    
    * bump to matterbridge 1.2.21

--- matterbridge/run.sh
+++ matterbridge/run.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/with-contenv bashio
 
-HOME_ASSISTANT_URL=$(bashio::config 'home_assistant_url')
-HOME_ASSISTANT_ACCESS_TOKEN=$(bashio::config 'home_assistant_access_token')
+HOME_ASSISTANT_URL=http://supervisor/core
+HOME_ASSISTANT_ACCESS_TOKEN=$SUPERVISOR_TOKEN
 HOME_ASSISTANT_CLIENT_CONFIG=$(bashio::config 'home_assistant_client_config')
 
 export HOME_ASSISTANT_URL
 export HOME_ASSISTANT_ACCESS_TOKEN
 export HOME_ASSISTANT_CLIENT_CONFIG
+
+# Add config on first run
+if [ ! -d /root/.matterbridge/storage ]; then
+    matterbridge -add matterbridge-home-assistant
+fi
 
 matterbridge -bridge
